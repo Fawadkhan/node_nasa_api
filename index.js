a37c607d55c32f252e23178c53e2b5639423dec6
fawadkhan <fawad.khan@wundermobility.com>
2023-09-10 00:29:52 +0200

    chore: initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+
+const result = []
+
+
+function isHabitable(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6;
+}
+fs.createReadStream('kepler.csv')
+    .pipe(parse({ comment: '#', columns: true }))
+    .on('data', (data) => {
+        if(isHabitable(data)) result.push(data);
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed', result.length, result.map(planet => planet['kepler_name']));
+    })
+    .on('error', (err) => {
+        console.log(err);
+    }
+    )
