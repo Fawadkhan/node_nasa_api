dbfde083b9c753df32afcc4b061cc32ac9701b8c
fawadkhan <fawad.khan@wundermobility.com>
2023-09-15 23:12:51 +0200

    chore: updated http

--- server/index.js
+++ server/index.js
@@ -1,20 +1,50 @@
 const http = require('http');
-const processCSV = require('../index.js');
 
 const server = http.createServer();
 
+const friends = [
+    {
+        id: 1,
+        name: 'Randy',
+    },
+    {
+        id: 2,
+        name: 'Mike',
+    },
+    {
+        id: 3,
+        name: 'Jhon',
+    },
+    {
+        id: 4,
+        name: 'Joe',
+    },
+]
+
 server.on('request', async (req, res) => {
-    
-    if(req.url === '/') {
-        try {
-            const result = await processCSV();
-            console.log("RESULT", result)
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(result.map(planet => planet['kepler_name']));
-        } catch (err) {
-            res.statusCode = 500;
-            res.end('Internal Server Error');
+    const items = req.url.split('/');
+    if(req.method === "POST" && items[1] === 'friends') { 
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log("REQUEST:", friend)
+            friends.push(JSON.parse(friend));
         }
+        );
+        req.pipe(res)
+    }
+    else if(req.method === "GET" && items[1] === 'friends') {
+            res.statusCode = 200;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            if(items.length === 3) {
+                const id = items[2];
+                const friend = friends[id - 1]
+                res.end(JSON.stringify(friend));
+            }
+            else {
+                res.end(JSON.stringify(friends));
+            }
+            res.end();
+        
     }
     else if(req.url === '/messages') {
         res.write('<p>HELLO IM JUST A PARAGRAPH</p>')
